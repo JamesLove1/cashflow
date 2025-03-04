69bf7696677622c5d2e247d489a2c2a684a45980
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-03-04 09:51:02 +0000

    update

--- buy_to_let_cashflow.py
+++ buy_to_let_cashflow.py
@@ -188,7 +188,6 @@ class cashflow():
 
         self.portfolio_disposal(self.df, Property_Location, startYear)
 
-
     def printDF(self):
         print(self.df.loc[:, "Valuation": ].head(5))
         print(self.df.loc[:, "Valuation": ].tail(5))        
@@ -198,9 +197,9 @@ cashflow(Monthly_Gross_Rent_GBP = 2750,
           Property_Location = "Greater London",                  # turn into an enum
           Annual_Ground_Rent_and_Service_Charge_GBP = 0,
           Property_Value_GBP = 650000,
-          Property_Type = "Apartment",                            # turn into an enum
-          Your_Place_of_Residency = "UK Resident",                # turn into an enum
-          What_Entity_is_Purchasing_the_Property = "Company",     # turn into an enum
+          Property_Type = "Apartment",                           # turn into an enum
+          Your_Place_of_Residency = "UK Resident",               # turn into an enum
+          What_Entity_is_Purchasing_the_Property = "Company",    # turn into an enum
           Are_you_financing_the_Property_with_a_Mortgage = "Yes" # turn into an enum
           ).printDF()
 
