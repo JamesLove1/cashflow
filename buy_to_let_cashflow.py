3abfab821abdda79e94c084227712507e479ae43
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-03-04 09:51:58 +0000

    update

--- buy_to_let_cashflow.py
+++ buy_to_let_cashflow.py
@@ -194,13 +194,13 @@ class cashflow():
 
 
 cashflow(Monthly_Gross_Rent_GBP = 2750,
-          Property_Location = "Greater London",                  # turn into an enum
+          Property_Location = "Greater London",                  
           Annual_Ground_Rent_and_Service_Charge_GBP = 0,
           Property_Value_GBP = 650000,
-          Property_Type = "Apartment",                           # turn into an enum
-          Your_Place_of_Residency = "UK Resident",               # turn into an enum
-          What_Entity_is_Purchasing_the_Property = "Company",    # turn into an enum
-          Are_you_financing_the_Property_with_a_Mortgage = "Yes" # turn into an enum
+          Property_Type = "Apartment",                           
+          Your_Place_of_Residency = "UK Resident",               
+          What_Entity_is_Purchasing_the_Property = "Company",    
+          Are_you_financing_the_Property_with_a_Mortgage = "Yes" 
           ).printDF()
 
 
